c64b77ded1144db30b8db6684cfe6215eb542e6b
ehghksvjscl <ehghkswodnjs@naver.com>
2022-11-10 13:33:57 +0900

    http로 표현하기 위한 - 단위 쪼개기-함수 추출하기

--- chapter1/example-code-1/ex1.js
+++ chapter1/example-code-1/ex1.js
@@ -1,13 +1,17 @@
 function statement(invoice, plays) {
-    let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
+    return renderPlainText(invoice, plays);
 
-    for (let perf of invoice.performances) {
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+    function renderPlainText(invoice, plays) {
+        let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
+
+        for (let perf of invoice.performances) {
+            result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+        }
+
+        result += `총액: ${usd(sumAmount())}\n`;
+        result += `적립 포인트: ${sumVolumeCredits()}점\n`;
+        return result;
     }
-
-    result += `총액: ${usd(sumAmount())}\n`;
-    result += `적립 포인트: ${sumVolumeCredits()}점\n`;
-    return result;
 
     function volumeCreditsFor(aPerformance) {
         volumeCredits = 0
