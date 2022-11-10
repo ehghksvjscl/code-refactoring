92836e01bbe7f1107dd7408a9a6dc858eed8ccaf
ehghksvjscl <ehghkswodnjs@naver.com>
2022-11-10 13:44:43 +0900

    http로 표현하기 - 중간 데이터 구조로 옮기기

--- chapter1/example-code-1/ex1.js
+++ chapter1/example-code-1/ex1.js
@@ -15,67 +15,67 @@ function statement(invoice, plays) {
         result += `총액: ${usd(sumAmount())}\n`;
         result += `적립 포인트: ${sumVolumeCredits()}점\n`;
         return result;
-    }
 
-    function volumeCreditsFor(aPerformance) {
-        volumeCredits = 0
-        volumeCredits += Math.max(aPerformance.audience - 30, 0);
-        // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if ("comedy" === playFor(aPerformance).type) volumeCredits += Math.floor(aPerformance.audience / 5);
+        function volumeCreditsFor(aPerformance) {
+            volumeCredits = 0
+            volumeCredits += Math.max(aPerformance.audience - 30, 0);
+            // 희극 관객 5명마다 추가 포인트를 제공한다.
+            if ("comedy" === playFor(aPerformance).type) volumeCredits += Math.floor(aPerformance.audience / 5);
 
-        return volumeCredits
-    }
-
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
-
-    function amountFor(aPerformance) {
-        let result = 0
-
-        switch (playFor(aPerformance).type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+            return volumeCredits
         }
 
-        return result
-    }
-
-    function usd(aNumber) {
-        return format = new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD",
-            minimumFractionDigits: 2
-        }).format(aNumber / 100);
-    }
-
-    function sumVolumeCredits() {
-        let result = 0;
-        for (let perf of invoice.performances) {
-            result += volumeCreditsFor(perf)
+        function playFor(aPerformance) {
+            return plays[aPerformance.playID];
         }
-        return result;
-    }
 
-    function sumAmount() {
-        let result = 0;
-        for (let perf of invoice.performances) {
-            result += amountFor(perf)
+        function amountFor(aPerformance) {
+            let result = 0
+
+            switch (playFor(aPerformance).type) {
+                case "tragedy":
+                    result = 40000;
+                    if (aPerformance.audience > 30) {
+                        result += 1000 * (aPerformance.audience - 30);
+                    }
+                    break;
+                case "comedy":
+                    result = 30000;
+                    if (aPerformance.audience > 20) {
+                        result += 10000 + 500 * (aPerformance.audience - 20);
+                    }
+                    result += 300 * aPerformance.audience;
+                    break;
+                default:
+                    throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+            }
+
+            return result
         }
-        return result
+
+        function usd(aNumber) {
+            return format = new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+                minimumFractionDigits: 2
+            }).format(aNumber / 100);
+        }
+
+        function sumVolumeCredits() {
+            let result = 0;
+            for (let perf of data.performances) {
+                result += volumeCreditsFor(perf)
+            }
+            return result;
+        }
+
+        function sumAmount() {
+            let result = 0;
+            for (let perf of data.performances) {
+                result += amountFor(perf)
+            }
+            return result
+        }
     }
 }
 
