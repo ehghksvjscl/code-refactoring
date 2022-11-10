531bde8315eac3a881f8de776324c27ccf7c5045
ehghksvjscl <ehghkswodnjs@naver.com>
2022-11-10 13:29:46 +0900

    적립 포인트 변수 제거하기

--- chapter1/example-code-1/ex1.js
+++ chapter1/example-code-1/ex1.js
@@ -1,18 +1,15 @@
 function statement(invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
 
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf)
-
         // 청구 내역을 출력한다.
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf)
     }
 
     result += `총액: ${usd(totalAmount)}\n`;
-    result += `적립 포인트: ${volumeCredits}점\n`;
+    result += `적립 포인트: ${sumVolumeCredits()}점\n`;
     return result;
 
     function volumeCreditsFor(aPerformance) {
@@ -60,6 +57,13 @@ function statement(invoice, plays) {
         }).format(aNumber / 100);
     }
 
+    function sumVolumeCredits() {
+        let volumeCredits = 0;
+        for (let perf of invoice.performances) {
+            volumeCredits += volumeCreditsFor(perf)
+        }
+        return volumeCredits;
+    }
 }
 
 module.exports = statement;
