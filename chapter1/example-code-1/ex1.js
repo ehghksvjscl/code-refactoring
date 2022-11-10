fe77cad2a364acfcfbc0b339bbecefde76bbafb3
ehghksvjscl <ehghkswodnjs@naver.com>
2022-11-10 14:20:05 +0900

    rnderPlainText() 안에서 amountFor()를 호출하던 부분을 중간 데이터를 사용하도록 바꾼다.

--- chapter1/example-code-1/ex1.js
+++ chapter1/example-code-1/ex1.js
@@ -5,6 +5,30 @@ function statement(invoice, plays) {
 
     return renderPlainText(statementData, plays);
 
+    function amountFor(aPerformance) {
+        let result = 0
+
+        switch (aPerformance.play.type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
+        }
+
+        return result
+    }
+
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }
@@ -12,6 +36,7 @@ function statement(invoice, plays) {
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
+        result.amount = amountFor(result);
         return result
     }
 
@@ -19,7 +44,7 @@ function statement(invoice, plays) {
         let result = `청구 내역 (고객명 : ${data.customer})\n`;
 
         for (let perf of data.performances) {
-            result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+            result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
         }
 
         result += `총액: ${usd(sumAmount())}\n`;
@@ -35,29 +60,7 @@ function statement(invoice, plays) {
             return volumeCredits
         }
 
-        function amountFor(aPerformance) {
-            let result = 0
 
-            switch (aPerformance.play.type) {
-                case "tragedy":
-                    result = 40000;
-                    if (aPerformance.audience > 30) {
-                        result += 1000 * (aPerformance.audience - 30);
-                    }
-                    break;
-                case "comedy":
-                    result = 30000;
-                    if (aPerformance.audience > 20) {
-                        result += 10000 + 500 * (aPerformance.audience - 20);
-                    }
-                    result += 300 * aPerformance.audience;
-                    break;
-                default:
-                    throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-            }
-
-            return result
-        }
 
         function usd(aNumber) {
             return format = new Intl.NumberFormat("en-US", {
@@ -78,7 +81,7 @@ function statement(invoice, plays) {
         function sumAmount() {
             let result = 0;
             for (let perf of data.performances) {
-                result += amountFor(perf)
+                result += perf.amount
             }
             return result
         }
