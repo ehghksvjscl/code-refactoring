2ba1815bece3b44b53843bfd6664ca12b7953920
ehghksvjscl <ehghkswodnjs@naver.com>
2022-11-10 17:23:52 +0900

    공연료 계산기 만들기 - 함수 선언 바꾸기

--- chapter1/example-code-1/createStatementData.js
+++ chapter1/example-code-1/createStatementData.js
@@ -1,6 +1,7 @@
 class PerformanceCalculator {
-    constructor(aPerformance) {
+    constructor(aPerformance, aPlay) {
         this.performance = aPerformance
+        this.play = aPlay
     }
 }
 
@@ -58,7 +59,7 @@ function createStatementData(invoice, plays) {
     }
 
     function enrichPerformance(aPerformance) {
-        const calculator = new PerformanceCalculator(aPerformance)
+        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = amountFor(result);
