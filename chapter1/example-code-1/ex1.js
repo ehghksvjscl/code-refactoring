adfb104e3d7911ffc4a117b88f0edc2191d2c4d9
ehghksvjscl <ehghkswodnjs@naver.com>
2022-11-10 14:15:50 +0900

    rnderPlainText() 안에서 playFor()를 호출하던 부분을 중간 데이터를 사용하도록 바꾼다.

--- chapter1/example-code-1/ex1.js
+++ chapter1/example-code-1/ex1.js
@@ -1,15 +1,25 @@
 function statement(invoice, plays) {
     const statementData = {}
     statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances;
+    statementData.performances = invoice.performances.map(enrichPerformance);
 
     return renderPlainText(statementData, plays);
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);
+        result.play = playFor(result);
+        return result
+    }
 
     function renderPlainText(data, plays) {
         let result = `청구 내역 (고객명 : ${data.customer})\n`;
 
         for (let perf of data.performances) {
-            result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+            result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         }
 
         result += `총액: ${usd(sumAmount())}\n`;
@@ -20,19 +30,15 @@ function statement(invoice, plays) {
             volumeCredits = 0
             volumeCredits += Math.max(aPerformance.audience - 30, 0);
             // 희극 관객 5명마다 추가 포인트를 제공한다.
-            if ("comedy" === playFor(aPerformance).type) volumeCredits += Math.floor(aPerformance.audience / 5);
+            if ("comedy" === aPerformance.play.type) volumeCredits += Math.floor(aPerformance.audience / 5);
 
             return volumeCredits
-        }
-
-        function playFor(aPerformance) {
-            return plays[aPerformance.playID];
         }
 
         function amountFor(aPerformance) {
             let result = 0
 
-            switch (playFor(aPerformance).type) {
+            switch (aPerformance.play.type) {
                 case "tragedy":
                     result = 40000;
                     if (aPerformance.audience > 30) {
@@ -47,7 +53,7 @@ function statement(invoice, plays) {
                     result += 300 * aPerformance.audience;
                     break;
                 default:
-                    throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+                    throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
             }
 
             return result
