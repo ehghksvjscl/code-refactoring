51f5eb3fb33c420531cbebd5eec3495233be1369
ehghksvjscl <ehghkswodnjs@naver.com>
2022-11-18 23:42:08 +0900

    change production test code

--- chapter4/samplecode.spec.js
+++ chapter4/samplecode.spec.js
@@ -12,3 +12,9 @@ test('province -> shortfall', () => {
 test('province -> profit', () => {
     expect(asia.profit).toEqual(230);
 });
+
+test('province -> change production', () => {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).toEqual(-6);
+    expect(asia.profit).toEqual(292);
+});
