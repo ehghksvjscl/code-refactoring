9bb61012b47ebb37854c0a8ce3105c9572c285ee
ehghksvjscl <ehghkswodnjs@naver.com>
2022-11-24 19:10:28 +0900

    첼린지 1 완료

--- ch6/6-11.js
+++ ch6/6-11.js
@@ -1,30 +1,101 @@
-export function priceOrder(product, quantity, shippingMethod) {
-  const basePrice = product.basePrice * quantity;
-  const discount =
-    Math.max(quantity - product.discountThreshold, 0) *
-    product.basePrice *
-    product.discountRate;
-  const shippingPerCase =
-    basePrice > shippingMethod.discountThreshold
-      ? shippingMethod.discountedFee
-      : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
-  return price;
+class Order {
+  #product;
+  #quantity;
+  #shippingMethod;
+  constructor(product, quantity, shippingMethod) {
+    this.#product = product;
+    this.#quantity = quantity;
+    this.#shippingMethod = shippingMethod;
+  }
+
+  get basePrice() {
+    return this.#quantity * this.#product.basePrice;
+  }
+
+  get discount() {
+    return Math.max(this.#quantity - this.#product.discountThreshold, 0) * 
+    this.#product.basePrice * 
+    this.#product.discountRate;
+  }
+
+  get shippingPerCase() {
+    return (this.basePrice > this.#shippingMethod.discountThreshold) ? 
+    this.#shippingMethod.discountedFee : this.#shippingMethod.feePerCase;
+  }
+
+  get shippingCost() {
+    return this.shippingPerCase * this.#quantity;
+  }
+
+  get  price() {
+    return this.basePrice - this.discount + this.shippingCost;
+  }
+
+}
+
+// 이렇게 해 놓고 파일을 쪼겠을 것이다.
+
+class Product {
+  #basePrice;
+  #discountRate;
+  #discountThreshold;
+  constructor(data) {
+    this.#basePrice = data.basePrice;
+    this.#discountRate = data.discountRate;
+    this.#discountThreshold = data.discountThreshold;
+  }
+
+  get basePrice() {
+    return this.#basePrice;
+  }
+
+  get discountRate() {
+    return this.#discountRate;
+  }
+
+  get discountThreshold() {
+    return this.#discountThreshold;
+  }
+  
 }
 
 // 사용 예:
-const product = {
+const product = new Product({
   basePrice: 10,
   discountRate: 0.1,
   discountThreshold: 10,
-};
+});
 
-const shippingMethod = {
+class ShippingMethod {
+  #feePerCase;
+  #discountedFee;
+  #discountThreshold;
+  constructor(data) {
+    this.#feePerCase = data.feePerCase;
+    this.#discountedFee = data.discountedFee;
+    this.#discountThreshold = data.discountThreshold;
+  }
+
+  get feePerCase() {
+    return this.#feePerCase;
+  }
+
+  get discountedFee() {
+    return this.#discountedFee;
+  }
+
+  get discountThreshold() {
+    return this.#discountThreshold;
+  }
+}
+
+// 이렇게 해 놓고 파일을 쪼겠을 것이다.
+
+const shippingMethod = new ShippingMethod({
   discountThreshold: 20,
   feePerCase: 5,
   discountedFee: 3,
-};
+});
 
-const price = priceOrder(product, 5, shippingMethod);
-console.log(price);
+const priceOrder = new Order(product, 5, shippingMethod);
+console.log(priceOrder.price);
