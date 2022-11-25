b8053f53f3a6ff6a93c31893fa5ebaeade7cfa66
ehghksvjscl <ehghkswodnjs@naver.com>
2022-11-25 16:08:19 +0900

    6-11-2.js 하는중

--- ch6/6-11-2.js
+++ ch6/6-11-2.js
@@ -1,18 +1,36 @@
 import fs from 'fs';
 
-if (!process.argv[2]) {
-  throw new Error('파일 이름을 입력하세요');
-}
+run(process.argv);
 
-const fileName = `./${process.argv[2]}.json`;
-if (!fs.existsSync(fileName)) {
-  throw new Error('파일이 존재하지 않습니다');
-}
+function run(args) {
 
-const rawData = fs.readFileSync(fileName);
-const orders = JSON.parse(rawData);
-if (process.argv.includes('-r')) {
-  console.log(orders.filter((order) => order.status === 'ready').length);
-} else {
-  console.log(orders.length);
+  parseCommend(args)
+
+  function parseCommend(args) {
+    if (!args[2]) {
+      throw new Error('파일 이름을 입력하세요');
+    }
+
+    const fileName = `./${args[2]}.json`;
+    if (!fs.existsSync(fileName)) {
+      throw new Error('파일이 존재하지 않습니다');
+    }
+  }
+  
+  function countOrder(fileName) {
+    const rawData = fs.readFileSync(fileName);
+    const orders = JSON.parse(rawData);
+    if (args.includes('-r')) {
+      console.log(orders.filter((order) => order.status === 'ready').length);
+    } else {
+      console.log(orders.length);
+    }
+  }
+  const rawData = fs.readFileSync(fileName);
+  const orders = JSON.parse(rawData);
+  if (args.includes('-r')) {
+    console.log(orders.filter((order) => order.status === 'ready').length);
+  } else {
+    console.log(orders.length);
+  }
 }
