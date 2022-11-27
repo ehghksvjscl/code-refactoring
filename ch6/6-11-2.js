d67bf77f7a92df411cb8b4f142d263ef13cfbecd
ehghksvjscl <ehghkswodnjs@naver.com>
2022-11-27 19:18:43 +0900

    쳅터 6공부 끝

--- ch6/6-11-2.js
+++ ch6/6-11-2.js
@@ -4,33 +4,31 @@ run(process.argv);
 
 function run(args) {
 
-  parseCommend(args)
+  countOrders(parseCommend(args))
+}
 
-  function parseCommend(args) {
-    if (!args[2]) {
-      throw new Error('파일 이름을 입력하세요');
-    }
-
-    const fileName = `./${args[2]}.json`;
-    if (!fs.existsSync(fileName)) {
-      throw new Error('파일이 존재하지 않습니다');
-    }
+function parseCommend(args) {
+  if (!args[2]) {
+    throw new Error('파일 이름을 입력하세요');
   }
+
+  const fileName = `./${args[2]}.json`;
+  if (!fs.existsSync(fileName)) {
+    throw new Error('파일이 존재하지 않습니다');
+  }
+
+  return {
+    fileName,
+    countReadyOnly: args.includes('-r')
+  }
+}
   
-  function countOrder(fileName) {
-    const rawData = fs.readFileSync(fileName);
-    const orders = JSON.parse(rawData);
-    if (args.includes('-r')) {
-      console.log(orders.filter((order) => order.status === 'ready').length);
-    } else {
-      console.log(orders.length);
-    }
-  }
+function countOrders({fileName, countReadyOnly}) {
   const rawData = fs.readFileSync(fileName);
   const orders = JSON.parse(rawData);
-  if (args.includes('-r')) {
-    console.log(orders.filter((order) => order.status === 'ready').length);
-  } else {
-    console.log(orders.length);
-  }
+  const filterd = countReadyOnly
+    ? orders.filter((order) => order.status === 'ready')
+    : orders;
+
+  console.log(filterd.length);
 }
