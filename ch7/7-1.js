2235dce1dce638aca340b03a2ed4e53d2c1138dc
ehghksvjscl <ehghkswodnjs@naver.com>
2022-11-29 23:53:15 +0900

    레코드 캡슐화 연습

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,5 +1,27 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+// const organization = { name: 'Acme Gooseberries', country: 'GB' };
 
-organization.name = 'Dream Coding';
+// organization.name = 'Dream Coding';
+// console.log(organization.name);
+// console.log(organization.country);
+
+class Organization {
+    #name;
+    #country;
+    constructor(data) {
+        this.#name = data.name;
+        this.#country = data.country;
+    }
+
+    get name() { return this.#name; }
+    set name(aString) { this.#name = aString; }
+    get country() { return this.#country; }
+    set country(aCountryCode) { this.#country = aCountryCode; }
+    get rawData() { return { name: this.name, country: this.country }; }
+}
+
+const organization = new Organization({ name: 'Acme Gooseberries', country: 'GB' })
+
 console.log(organization.name);
 console.log(organization.country);
+organization.name = 'Dream Coding';
+console.log(organization.rawData);
