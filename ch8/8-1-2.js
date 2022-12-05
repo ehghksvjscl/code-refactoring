cec16a9c8c21e97e0a1aab20f0e2bd887be5da14
ehghksvjscl <ehghkswodnjs@naver.com>
2022-12-05 18:41:54 +0900

    함수 옮기기 .. 이건 조금 어려운데?

--- ch8/8-1-2.js
+++ ch8/8-1-2.js
@@ -4,22 +4,27 @@ export class Account {
     this._daysOverdrawn = daysOverdrawn;
   }
 
+  // 은행 수수료
   get bankCharge() {
     let result = 4.5;
     if (this._daysOverdrawn > 0) result += this.overdraftCharge;
     return result;
   }
 
+  // 한도 수수료
   get overdraftCharge() {
-    if (this.type.isPremium) {
-      const baseCharge = 10;
-      if (this._daysOverdrawn <= 7) return baseCharge;
-      else return baseCharge + (this._daysOverdrawn - 7) * 0.85;
-    } else return this._daysOverdrawn * 1.75;
+    if (!this.isPremium) {return this._daysOverdrawn * 1.75}
+    const baseCharge = 10;
+    return (this._daysOverdrawn <= 7) ? baseCharge : baseCharge + (this._daysOverdrawn - 7) * 0.85;
   }
-
+  
+  //일별 인출 한도
   get daysOverdrawn() {
     return this._daysOverdrawn;
+  }
+
+  get isPremium() {
+    return this.accountType.type === 'Premium';
   }
 }
 
@@ -27,7 +32,9 @@ export class AccountType {
   constructor(type) {
     this._type = type;
   }
-  get isPremium() {
-    return this._type === 'Premium';
+
+  get type() {
+    return this._type;
   }
+
 }
