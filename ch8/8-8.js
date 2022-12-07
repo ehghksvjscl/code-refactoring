f547ed727a6b6e6379c74d0cd348b6bb0452d883
ehghksvjscl <ehghkswodnjs@naver.com>
2022-12-07 13:31:38 +0900

    반복문을 파이프라인으로 바꾸기

--- ch8/8-8.js
+++ ch8/8-8.js
@@ -1,19 +1,13 @@
 export function acquireData(input) {
-  const lines = input.split('\n');
-  let firstLine = true;
-  const result = [];
-  for (const line of lines) {
-    if (firstLine) {
-      firstLine = false;
-      continue;
-    }
-    if (line.trim() === '') continue;
-    const record = line.split(',');
-    if (record[1].trim() === 'India') {
-      result.push({ city: record[0].trim(), phone: record[2].trim() });
-    }
-  }
-  return result;
+  return input.split('\n')
+    .slice(1)
+    .filter(line => line.trim() !== '')
+    .map(line => line.split(','))
+    .filter(fields => fields[1].trim() === 'India')
+    .map(line => { return {
+      city: line[0].trim(),
+      phone: line[2].trim(),
+    }})
 }
 
 const input = `office, country, telephone\n
