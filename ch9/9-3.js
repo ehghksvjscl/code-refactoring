dc0cb41377624bc5d4b340eaf5d0701b82d8966d
ehghksvjscl <ehghkswodnjs@naver.com>
2022-12-09 17:43:19 +0900

    파생 변수를 질의 함수로 바꾸기

--- ch9/9-3.js
+++ ch9/9-3.js
@@ -2,23 +2,25 @@
 class Order {
   // 다른 코드 있다고 가정
   get discountedTotal() {
-    return this._discountedTotal;
+    return this.basePrice - this.discount;
   }
   set discount(value) {
-    const old = this._discount;
-    this._discount = value;
-    this._discountedTotal += old - value;
+    this._discount = value; // get 
   }
 }
 
 // 예제 2
 class ProductionPlan {
   // 다른 코드 있다고 가정
-  get production() {
-    return this._production;
+  get totalProduction() {
+    return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
   }
+
+  set addAdjustments(args) {
+    this._adjustments.push(args);
+  }
+  
   applyAdjustment(adjustment) {
-    this._adjustments.push(adjustment);
-    this._production += adjustment.amount;
+    this.addAdjustments(adjustment);
   }
 }
