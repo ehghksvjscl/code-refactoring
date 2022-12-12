a5cae1103e1d55a8e6d14f40babe6fb3b67080c8
ehghksvjscl <ehghkswodnjs@naver.com>
2022-12-12 18:47:48 +0900

    조건식 통합하기

--- ch10/10-2.js
+++ ch10/10-2.js
@@ -1,6 +1,10 @@
 function disabilityAmount(employee) {
-  if (employee.seniority < 2) return 0;
-  if (employee.monthsDisabled > 12) return 0;
-  if (employee.isPartTime) return 0;
+  if (isNotGetDisabilityAmount(employee)) {
+    return 0
+  }
   return 1;
+}
+
+function isNotGetDisabilityAmount(employee) {
+  return employee.seniority < 2 || employee.monthsDisabled > 12 || employee.isPartTime;
 }
