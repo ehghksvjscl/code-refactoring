a4b943beef7052efe8c2e7a4b5a9421bcc01eff2
ehghksvjscl <ehghkswodnjs@naver.com>
2022-12-13 12:06:16 +0900

    중첩 조건문을 보호 구문으로 바꾸기

--- ch10/10-3-2.js
+++ ch10/10-3-2.js
@@ -1,11 +1,12 @@
 export function adjustedCapital(instrument) {
-  let result = 0;
-  if (instrument.capital > 0) {
-    if (instrument.interestRate > 0 && instrument.duration > 0) {
-      result =
-        (instrument.income / instrument.duration) *
-        anInstrument.adjustmentFactor;
-    }
+  if (!isAdjustedCapital) {
+      return 0
   }
-  return result;
+    return (instrument.income / instrument.duration) * anInstrument.adjustmentFactor;
+}
+
+function isAdjustedCapital() {
+  return (instrument.capital > 0 
+         && instrument.interestRate > 0 
+         && instrument.duration > 0)
 }
