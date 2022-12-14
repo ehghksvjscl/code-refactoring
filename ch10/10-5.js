83c73a829ff29d6e3d22364456d9e8a968271020
ehghksvjscl <ehghkswodnjs@naver.com>
2022-12-14 11:54:44 +0900

    특이 케이스 추가하기

--- ch10/10-5.js
+++ ch10/10-5.js
@@ -1,3 +1,52 @@
+class Hotel {
+  constructor() {
+    this.rooms = []
+  }
+
+  addRoom(roomNumber) {
+    this.rooms.push(new Room(roomNumber))
+  }
+
+  emptyRoom(roomNumber) {
+    this.rooms[roomNumber] = new EmptyRoom(roomNumber)
+  }
+
+
+  cleanRooms() {
+    console.log("모든 방을 청소합니다!")
+    this.rooms.forEach(room => room.clean())
+  }
+}
+
+class Room {
+  constructor(roomNumber) {
+    this.roomNumber = roomNumber
+  }
+
+  clean() {
+    console.log(`${this.roomNumber} 방을 청소 합니다!`)
+  }
+}
+
+class EmptyRoom extends Room {
+  constructor(roomNumber) {
+    super(roomNumber)
+  }
+
+  clean() {
+    console.log(`${this.roomNumber} 방은 비어있습니다!`)
+  }
+}
+
+const hotel = new Hotel()
+hotel.addRoom(0)
+hotel.addRoom(1)
+hotel.cleanRooms()
+hotel.emptyRoom(1)
+hotel.cleanRooms()
+
+
+
 export class Site {
   constructor(customer) {
     this._customer = customer;
@@ -30,13 +79,19 @@ export class Customer {
   }
 }
 
+export class UnknownCustomer extends Customer {
+  constructor() {
+    super('occupant');
+  }
+}
+
 // 사용하는 부분
 export function customerName(site) {
   const aCustomer = site.customer;
   // 더 많은 코드가 여기에 있음
-  let customerName;
-  if (aCustomer === 'unknown') customerName = 'occupant';
-  else customerName = aCustomer.name;
+  if (aCustomer === 'unknown') {
+    return new UnknownCustomer().name;
+  }  
 
-  return customerName;
+  return aCustomer.name;
 }
