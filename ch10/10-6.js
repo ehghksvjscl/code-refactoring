1c81daac0e56c0e4ac7f497bc5f64363685560de
ehghksvjscl <ehghkswodnjs@naver.com>
2022-12-14 13:27:44 +0900

    어서션 추가하기

--- ch10/10-6.js
+++ ch10/10-6.js
@@ -1,8 +1,15 @@
+import {strict as assert } from 'assert';
+
 class Customer {
   constructor() {
     this.discountRate = 0;
   }
   applyDiscount(number) {
+    // 정말 number가 0 밑으로 떨어지면 안되는 값일 경우
+    assert(number >= 0);
     return this.discountRate ? number - this.discountRate * number : number;
   }
 }
+
+const customer = new Customer();
+customer.applyDiscount(1)
