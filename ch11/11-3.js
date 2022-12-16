15302ff3946d27207aa88f1828625796abe80bd6
ehghksvjscl <ehghkswodnjs@naver.com>
2022-12-16 18:40:03 +0900

    플레그 인수 제거하기

--- ch11/11-3.js
+++ ch11/11-3.js
@@ -9,11 +9,31 @@ function setDimension(name, value) {
     return;
   }
 }
+// 위에 상황을 만들지 말자.!!!!!
+
+function setHeight(value) {
+  this._height = value;
+}
+
+function setWidth(value) {
+  this._width = value;
+}
+
 
 // 예제 2
 class Concert {
-  book(customer, isPremium) {}
+  regularBook(customer) {
+    this.#book(customer, false);
+  }
+  premiumBook(customer) {
+    this.#book(customer, true);
+  }
+
+  #book(customer, isPremium) {}
+  // 꼭 토글이 필요할 때는 Private 필드를 사용하자.
 }
 
 // 예제 3
-function setSwitch(on);
+function setSwitch(on); // 좋지 않다. 
+function switchOn()
+function switchOff()
