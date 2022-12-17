7bd58d3b6c3aca1082f42c7ebf0fbd9e5766057c
ehghksvjscl <ehghkswodnjs@naver.com>
2022-12-17 15:48:17 +0900

    생성자를 펙터리 함수로 바꾸기

--- ch11/11-8.js
+++ ch11/11-8.js
@@ -1,3 +1,7 @@
+// 사용자 입장에서는 typeCode라는 것에 뭐가 들어가야 하는지 잘 모른다
+// 그래서 팩토리 함수를 생성해서 자동으로 생성 되게 하는 것이 좋다.
+// 단 이따 생성자는 있으면 안된다
+
 export class Employee {
   constructor(name, typeCode) {
     this._name = name;
@@ -14,4 +18,19 @@ export class Employee {
   static get legalTypeCodes() {
     return { E: 'Engineer', M: 'Manager', S: 'Salesman' };
   }
+
+  static createEngineer(name) {
+    return new Employee(name, 'E')
+  }
+
+  static createSineerEngineer(name) {
+    return new Employee(name, 'SE')
+  }
+
+  static createMaketer(name) {
+    return new Employee(name, 'M')
+  }
 }
+
+// 외부에서 호출 할 때
+Employee.createEngineer("우노")
