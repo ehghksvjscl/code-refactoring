63ebadf158951b9ee7cb8efd503944cd5b1cf41c
ehghksvjscl <ehghkswodnjs@naver.com>
2022-12-23 14:25:29 +0900

    에러 코드를 리턴 하는 형태 대신 명확한 에러를 떤지자!

--- ch11/11-12.js
+++ ch11/11-12.js
@@ -1,4 +1,21 @@
+// 에러를 반환하는 경우 명확하게 
+
 function localShippingRules(data) {
   if (data) return new ShippingRules(data);
-  else return -23;
+  else throw new OrderProcessingError(-23);
+}
+
+class OrderProcessingError extends Error {
+  constructor(errorCode) {
+    super()
+    this.errorCode = errorCode;
+  }
+}
+
+try {
+  const shippingRules = localShippingRules();
+} catch (error) {
+  if (error instanceof OrderProcessingError) {
+    console.log(error);
+  }
 }
