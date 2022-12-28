2f47ef5779b2b13adebf57d75aa5d5f6454ee182
ehghksvjscl <ehghkswodnjs@naver.com>
2022-12-28 18:52:44 +0900

    타입 코드를 서브클래스로 바꾸기

--- ch12/12-6.js
+++ ch12/12-6.js
@@ -1,26 +1,38 @@
 class Employee {
   #name;
-  #type;
-  constructor(name, type) {
-    this.validateType(type);
+  constructor(name) {
     this.#name = name;
-    this.#type = type;
-  }
-
-  validateType(arg) {
-    if (!['engineer', 'manager', 'salesperson'].includes(arg)) {
-      throw new Error(`${arg}라는 직원 유형은 없습니다.`);
-    }
   }
 
   get type() {
-    return this.#type;
+    return 'Employee'
   }
 
   toString() {
-    return `${this.#name} (${this.type})`;
+    return `${this.#name}`;
+  }
+
+  // 펙토리 함수를 사용해서 만들어도 된다(type을 무조건 받아야 하는 경우)
+  // static createEngineer(name, type) {
+  //   return new Engineer(name, type);
+  // }
+}
+
+class Engineer extends Employee {
+  get type() {
+    return 'Engineer';
+  }
+}
+class Manager extends Employee {
+  get type() {
+    return 'Manager';
+  }
+}
+class Salesperson extends Employee {
+  get type() {
+    return 'Salesperson';
   }
 }
 
-const ellie = new Employee('엘리', 'engineer');
-const bob = new Employee('밥', 'manager');
+const ellie = new Engineer('엘리');
+const bob = new Manager('밥');
