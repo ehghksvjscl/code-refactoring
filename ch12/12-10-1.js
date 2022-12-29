5097c4ed2defbf9846e1e1fee5aa159fc3e7d294
ehghksvjscl <ehghkswodnjs@naver.com>
2022-12-29 16:29:14 +0900

    서브클레스를 위임으로 바꾸기

--- ch12/12-10-1.js
+++ ch12/12-10-1.js
@@ -1,42 +1,74 @@
 class Booking {
-  constructor(show, date) {
-    this._show = show;
-    this._date = date;
+  #show;
+  #date;
+  constructor(show, date, extras) {
+    this.#show = show;
+    this.#date = date;
   }
 
   get hasTalkback() {
-    return this._show.hasOwnProperty('talkback') && !this.isPeakDay;
+    return this.#premiumDelegate
+    ? this.#premiumDelegate.hasTalkback
+    : this.#show.hasOwnProperty('talkback') && !this.isPeakDay;
   }
 
   get basePrice() {
-    let result = this._show.price;
+    let result = this.#show.price;
 
     if (this.isPeakDay) {
       result += Math.round(result * 0.15);
     }
 
+    return this.#premiumDelegate
+    ? this.#premiumDelegate.extendBasePrice(result)
+    : result;
+  }
+
+  get hasDinner() {
+    return this.#premiumDelegate
+    ? this.#premiumDelegate.hasDinner
+    : undefined;
+  }
+
+  #bePremium(extras) {
+    this.#premiumDelegate = new PremiumBookingDelegate(this, extras);
+  }
+
+  createBooking(show, date) {
+    return new Booking(show, date);
+  }
+
+  createPremiumBooking(show, date, extras) {
+    const result = new PremiumBooking(show, date, extras);
+    result.#bePremium(extras);
     return result;
   }
 }
 
-class PremiumBooking extends Booking {
-  constructor(show, date, extras) {
-    super(show, date);
-    this._extras = extras;
+class PremiumBookingDelegate {
+  #host;
+  #extras;
+  constructor(host, extras) {
+    this.#host = host;
+    this.extras = extras;
   }
 
   get hasTalkback() {
-    return this._show.hasOwnProperty('talkback');
+    return this.#host.#show.hasOwnProperty('talkback');
   }
 
   get basePrice() {
-    return Math.round(super.basePrice + this._extras.PremiumFee);
+    return Math.round(super.basePrice + this.#extras.PremiumFee);
   }
 
   get hasDinner() {
-    return this._extras.hasOwnProperty('dinner') && !this.isPeakDay;
+    return this.#extras.hasOwnProperty('dinner') && !this.isPeakDay;
+  }
+
+  extendBasePrice(base) {
+    return base + this.#extras.premiumFee;
   }
 }
 
-const booking = new Booking(show, date);
-const premiumBooking = new PremiumBooking(show, date, extras);
+const booking = Booking.createBooking(show, date);
+const premiumBooking = Booking.createPremiumBooking(show, date, extras);
