079ba6b105fb91a792951e7648051b98ec06a153
ehghksvjscl <ehghkswodnjs@naver.com>
2022-12-30 01:52:47 +0900

    reset

--- challenge/statement.js
+++ challenge/statement.js
@@ -1,67 +1,16 @@
-// 1. 소스코드를 이해 하자
-// 2. 이해한 소스코드를 기능별로 나누어 보자
-// 3. OOP로 리팩토링 하자
-
-// 단어 정리 (영어)
-// statement: 명세서
-// invoice: 청구서
-// play: 연극
-// performance: 공연
-// audience: 관객
-// volumeCredits: 포인트
-
-class Statement {
-  constructor(invoice, plays) {
-    this.invoice = invoice;
-    this.plays = plays;
-  }
-
-  get result() {}
-}
-
-function format(data) {
-  return new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format(data);
-}
-
-class Play {
-  get totalAmount() {}
-  get totalVolumeCredits() {}
-
-  isAudience
-}
-
-class Tragedy extends Play {}
-
-class Comedy extends Play {}
-
-class UnknownPlayError extends Error {}
-
-
-function createPlay(data) {
-  switch (data.type) {
-    case 'tragedy': // 비극
-      return new c(data);
-    case 'comedy': // 희극
-      return new ComedyPlay(data);
-    default:
-      throw new Error(`알 수 없는 장르: ${data.type}`); // refeactoring
-  }
-}
-
-
 export function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+  const format = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format;
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = 0;
-
     switch (play.type) {
       case 'tragedy': // 비극
         thisAmount = 40000;
@@ -79,12 +28,10 @@ export function statement(invoice, plays) {
       default:
         throw new Error(`알 수 없는 장르: ${play.type}`);
     }
-
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트를 제공한다.
     if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
-
     // 청구 내역을 출력한다.
     result += `  ${play.name}: ${format(thisAmount / 100)} (${
       perf.audience
@@ -95,14 +42,12 @@ export function statement(invoice, plays) {
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 }
-
 // 사용예:
 const playsJSON = {
   hamlet: { name: 'Hamlet', type: 'tragedy' },
   'as-like': { name: 'As You Like It', type: 'comedy' },
   othello: { name: 'Othello', type: 'tragedy' },
 };
-
 const invoicesJSON = [
   {
     customer: 'BigCo',
@@ -122,7 +67,6 @@ const invoicesJSON = [
     ],
   },
 ];
-
 const result = statement(invoicesJSON[0], playsJSON);
 const expected =
   '청구 내역 (고객명: BigCo)\n' +
